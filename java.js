6c0edf3ccf9134f1944300b2d497f816d927b153
Grishma Ptael <patelgrishma9911@gmail.com>
2019-08-06 20:49:39 -0400

    changedjavalink

--- java.js
+++ java.js
@@ -1,6 +1,21 @@
-$(".button1").on("click", function() {
-    $("#slide2").show() 
+$(document).ready(function() {
+
+    $("#search").on("click", function() {
+        $("#slide2").show() 
+        $("#slide1").hide()
+        
+        console.log(this);
+    });
+    
     // $("#slide1").hide()
 });
 
-// $("#slide2").hide()
+$("#search2").on("click", function() {
+    $("#slide3").show()
+    $("#slide2").hide()
+    $("#slide1").hide()
+
+    console.log(this);
+});
+
+$("#slide2").hide()
