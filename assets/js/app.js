1fd4dda5cda299f294b7fa93c1b0f42b2b3363cf
prashanti <iawwfte@gmail.com>
2019-08-13 19:40:01 -0400

    link mapping style and added divs

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,12 +13,14 @@ function initMap() {
       };
       
       infoWindow.setPosition(pos);
-      infoWindow.setContent('We found your location. The search will populate phsycians within a 5 mile radius.');
+      infoWindow.setContent('We found your location. The search will populate phsycians within a 25 mile radius.');
       infoWindow.open(map);
       map.setCenter(pos);
+
       $(document).ready(function () {
         $("#button").on("click", function () {
-          $("#slide2").show()
+          $("#slide1").show()
+          $("#results-div").empty();
           getDocotorInfo();
       
           function getDocotorInfo() {
@@ -28,7 +30,7 @@ function initMap() {
             var lng = pos.lng;
             var lat = pos.lat;
             var api_key = 'a6adddec4df7db9f9b37cd18dbb4a61e';
-            var queryURL = 'https://api.betterdoctor.com/2016-03-01/doctors?location=' + lat + ',' + lng + ',4&skip=2&limit=5&query=' + specialties + ' &user_key=' + api_key;
+            var queryURL = 'https://api.betterdoctor.com/2016-03-01/doctors?location=' + lat + ',' + lng + ',25&skip=0&limit=5&query=' + specialties + ' &user_key=' + api_key;
      
             $.ajax({
               url: queryURL,
@@ -37,32 +39,35 @@ function initMap() {
       
               console.log(response);
               
-      
-      
-      
               for (var i = 0; i < response.data.length; i++) {
-                // $("#results-div").empty();
+                
                 var docResults = $('#results-div');
       
-                if (response.data[i].practices[0].name != "undefined" && response.data[i].practices[0].distance < 5) {
+                if (response.data[i].practices[0].name != "undefined" && response.data[i].practices[0].distance < 25) {
                   //==============================[ defines variables for response data ]==============================//
                   var docSpec = response.data[i].specialties[0].actor;
                   var docName = response.data[i].practices[0].name;
                   var docStreet = response.data[i].practices[0].visit_address.street;
                   var docCity = response.data[i].practices[0].visit_address.city;
                   var docZip = response.data[i].practices[0].visit_address.zip;
+                  var docImage = response.data[i].profile.image_url;
+                  // var docPic = $("<img>").attr("src", docImage)
+                  
       
                   //=======================================[ console logs ]==========================================//
                   // console.log("data practices length num =" + i + " : " + response.data[i].practices.length)
-                  console.log("name: " + response.data[i].practices[0].name);
-                  console.log("address: " + response.data[i].practices[0].visit_address.street);
-                  console.log(docStreet + docCity + docZip);
-      
-                  docResults.append(docSpec + '<br />' + docName + '<br />' + docStreet + '<br />' + docCity + ', ' + docZip + '<br /><br />');
-      
+                  // console.log("name: " + response.data[i].practices[0].name);
+                  // console.log("address: " + response.data[i].practices[0].visit_address.street);
+                  // console.log(docStreet + docCity + docZip)
+                  
+
+                  //=======================================[ constructing html ]==========================================//
+
+                  docResults.append('<img src='+ docImage +'></img>' + '<br />' + docSpec + '<br />' + docName + '<br />' + docStreet + '<br />' + docCity + ', ' + docZip + '<br /><br />');
                 }
-      
+                
               }
+               
             });
       
           }
@@ -86,3 +91,28 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
 }
+
+var api_key = 'a6adddec4df7db9f9b37cd18dbb4a61e';
+var queryURL = 'https://api.betterdoctor.com/2016-03-01/conditions?user_key=' + api_key;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+
+  //  console.log(response);
+
+    for (var i = 0; i < response.data.length; i++) {
+        // $("#conditions-div").empty();
+        // var conditions = $('#conditions-div');
+
+        // conditions.append(response.data[i].name);
+    }
+
+    $(document).ready(function () {
+        var conditions = $('select');
+        for (var i = 0; i < response.data.length; i++) {
+            conditions.append('<option value="' + response.data[i].name + '">' + response.data[i].name + '</option>');
+        }
+    });
+});
